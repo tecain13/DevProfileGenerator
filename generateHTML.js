66b086ad68660ff583ca94fc8c9196b7774f13a3
Tyler Cain <tecain13@gmail.com>
2020-01-18 23:14:48 -0700

    repo info staged for extraction

--- generateHTML.js
+++ generateHTML.js
@@ -1,32 +1,32 @@
 const colors = {
-    green: {
-        wrapperBackground: "#E6E1C3",
-        headerBackground: "#C1C72C",
-        headerColor: "black",
-        photoBorderColor: "#black"
-    },
-    blue: {
-        wrapperBackground: "#5F64D3",
-        headerBackground: "#26175A",
-        headerColor: "white",
-        photoBorderColor: "#73448C"
-    },
-    pink: {
-        wrapperBackground: "#879CDF",
-        headerBackground: "#FF8374",
-        headerColor: "white",
-        photoBorderColor: "#FEE24C"
-    },
-    red: {
-        wrapperBackground: "#DE9967",
-        headerBackground: "#870603",
-        headerColor: "white",
-        photoBorderColor: "white"
-    }
+  green: {
+    wrapperBackground: "#E6E1C3",
+    headerBackground: "#C1C72C",
+    headerColor: "black",
+    photoBorderColor: "#black"
+  },
+  blue: {
+    wrapperBackground: "#5F64D3",
+    headerBackground: "#26175A",
+    headerColor: "white",
+    photoBorderColor: "#73448C"
+  },
+  pink: {
+    wrapperBackground: "#879CDF",
+    headerBackground: "#FF8374",
+    headerColor: "white",
+    photoBorderColor: "#FEE24C"
+  },
+  red: {
+    wrapperBackground: "#DE9967",
+    headerBackground: "#870603",
+    headerColor: "white",
+    photoBorderColor: "white"
+  }
 };
 
 function generateHTML(data) {
-    return `<!DOCTYPE html>
+  return `<!DOCTYPE html>
   <html lang="en">
      <head>
         <meta charset="UTF-8" />
@@ -170,6 +170,19 @@ function generateHTML(data) {
               zoom: .75; 
             } 
            }
-        </style>`
-}
+        </style>
+        
+        </head>
+        
+        <body>
+        
+        ${data.name}
+        ${data.}
 
+        </body>
+        </html>`
+
+
+};
+
+module.exports = generateHTML; 
--- index.js
+++ index.js
@@ -1,14 +1,56 @@
-const questions = [
+var inquirer = require("inquirer");
+var axios = require("axios");
+var generateHTML = require("./generateHTML");
+var electron = require("electron-html-to");
 
+const questions = [
+    {
+        type: "input",
+        message: "Hi there. What is your favorite color?",
+        name: "color"
+    },
+
+    {
+        type: "input",
+        message: "What is your Github username?",
+        name: "githubprofile"
+    }
 ];
+
 
 function writeToFile(fileName, data) {
 
 }
 
 function init() {
+    inquirer.prompt(questions).then(function (response) {
+        console.log(response.color);
+        console.log(response.githubprofile);
+        const url = `https://api.github.com/users/${response.githubprofile}`
+        axios.get(url).then(function (data) {
+            // console.log(data);
+            const urlstars = `https://api.github.com/users/${response.githubprofile}/repos`
+            axios.get(urlstars).then(function (repos) {
+                var stars = 0;
+                for (var i = 0; i < repos.data.length; i++) {
+                    stars += repos.data[i].stargazers_count;
+                }
+                console.log(stars);
+                const color = response.color;
+                const name = data.data.name;
+                const location = data.data.location;
+                //and keep adding within the squiggly brackets
+                console.log(name);
+                const html = generateHTML({ stars, color, name })
+                console.log(html);
+            })
+                .catch(function (error) {
+                    console.log(error)
+                });
+        })
+    })
 
-
-    
-    init();
 };
+
+
+init();
